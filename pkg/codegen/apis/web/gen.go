318a3acb8b5f64c6aaa428388eb587d37429cf60
Jacob Brewer <Jacob.brewer@bthree.uk>
2025-01-16 13:12:17 +0000

    removing test package api

--- pkg/codegen/apis/web/gen.go
+++
@@ -1,5 +0,0 @@
-package api
-
-//go:generate oapi-codegen -generate types -package api -templates ../../templates -o types.go -config ../../oapi-config.yaml ./routes.yaml
-//go:generate oapi-codegen -generate gorilla -package api -templates ../../templates -o server.go -config ../../oapi-config.yaml ./routes.yaml
-//go:generate oapi-codegen -generate client -package api -templates ../../templates -o client.go -config ../../oapi-config.yaml ./routes.yaml
